cfce72b15b7871e02928c8b5aeb7efb3587e401e
MahmoudHaouachi <mahmoud.haouachi@gmail.com>
2018-05-12 16:51:59 +0600

    PhotoSharingInitializer

---
+++ PhotoSharingApp/Models/PhotoSharingInitializer.cs
@@ -0,0 +1,24 @@
+﻿using PhotoSharingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApp.Models
+{
+    public class PhotoSharingInitializer: DropCreateDatabaseAlways<PhotoSharingContext> 
+    {
+        protected override void Seed(PhotoSharingContext context)
+        {
+            List<Photo> photo = new List<Photo> () ;
+            Photo p = new Photo();
+            p.Title = "Test Photo";
+            p.Description = "This is a test";
+            p.Owner = "NaokiSato";
+            p.PhotoFile = getFileBytes("\\Images\\");
+            p.CreatedDate = DateTime.Now;
+            p.ImageMimeType = "image/jpeg";
+        }
+    }
+}
