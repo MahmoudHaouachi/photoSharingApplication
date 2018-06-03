f6d0f0f43081b2bfc6f85a10027b4eb1f997acbd
MahmoudHaouachi <mahmoud.haouachi@gmail.com>
2018-06-03 18:19:14 +0600

    Animate the photo cards in the slideshow, Success!

---
+++ PhotoSharingApp/Scripts/SlideShow.js
@@ -0,0 +1,23 @@
+﻿var percentIncrement;
+var percentCurrent = 100;
+
+function slideSwitch() {
+    var $activeCard = $('#slide-show DIV.active-card');
+    if ($activeCard.length == 0) {
+        $activeCard = $('#slide-show DIV.slide-show-card:last');
+    }
+        var $nextCard = $activeCard.next();
+        if ($nextCard.length == 0) {
+            $nextCard = $('#slide-show DIV.slide-show-card:first');
+        }
+        $activeCard.addClass('last-active-card');
+        $nextCard.css({ opacity: 0.0 });
+        $nextCard.addClass('active-card');
+        $nextCard.animate({ opacity: 1.0 }, 1000, function () {
+            $activeCard.removeClass('active-card last-active-card');
+        });
+        $(document).ready(function () {
+            setInterval("slideSwitch()", 5000);
+        });
+    
+}
